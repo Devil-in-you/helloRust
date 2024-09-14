62da59a4030e9b225563e547d56e6af55044b584
Devil-in-you <prkashved2@gmail.com>
2024-09-14 23:11:20 +0530

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
  */
 
 
-#[allow(non_snake_case)]
+#[allow(non_snake_case)]// if you want to ignore the snake case warning
 use std::{i8, i16, i32, i64, i128, u8, u16, u32, u64, isize, usize, f32, f64};
 
 use std::io::stdin;
@@ -21,4 +21,25 @@ fn main() {
 
     let  mut num = 100;
     println!("the number is {}", num);
+
+    //Boolean
+    let is_it_true: bool = true; //false
+
+    //Character
+    let one_char: char = 'a'; //single quotes not double quotes
+
+    //place variable in placeholders
+    println!("my name is {} {}, and my fav char is {}", name, sur_name, one_char);
+
+    //Define multiple variables on multiple lines
+    let(country, city) = ("India", "Bhopal");
+    println!("I am from {} and I live in {}", country, city);
+
+    //Operators for math calculations
+    println!("5 + 4 = {}", 5 + 4);
+    println!("5 - 4 = {}", 5 - 4);
+    println!("5 * 4 = {}", 5 * 4);
+    println!("5 / 4 = {}", 5 / 4);
+    println!("5 % 4 = {}", 5 % 4);
+
 }
