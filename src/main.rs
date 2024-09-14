7b360540fe5f8a37c6cfd00ac07f0a02a671b65a
Devil-in-you <prkashved2@gmail.com>
2024-09-14 17:10:58 +0530

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::{i8, i16, i32, i64, i128, u8, u16, u32, u64, isize, usize, f32, f64};
+
+use std::io::stdin;
+
 fn main() {
     println!("Hello, Rust !!!");
+
+    let num: i8 = 10; //i32 by default
+    println!("this number is {}", num);
 }
